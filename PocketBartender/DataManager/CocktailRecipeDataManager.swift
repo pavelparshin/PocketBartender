18e2e7d846201d0d1f6ee5e7b6e97492a1874c41
Pavel Parshin <mail@pavelparshin.ru>
2020-06-12 22:27:44 +0300

    create structure and data

---
+++ PocketBartender/DataManager/CocktailRecipeDataManager.swift
@@ -0,0 +1,28 @@
+//
+//  CocktailRecipeDataManager.swift
+//  PocketBartender
+//
+//  Created by Pavel Parshin on 12.06.2020.
+//  Copyright © 2020 Pavel Parshin. All rights reserved.
+//
+
+import Foundation
+
+class CocktailRecipeDataManager {
+    
+    static let shared = CocktailRecipeDataManager()
+    
+    private init() {}
+    
+    var recipe: [CoctailModel] = [
+        CoctailModel(name: "Пина Колада", alcohol: "Белый Ром", amountOfAlcohol: 30,
+                     ingredients: ["ананасовый сок", "кокосовое молоко", "сливки", "лёд"],
+                     descrpiption: "Карибский слабоалкогольный коктейль, в котором гармонично сочетаются ром, ананасовый сок и кокосовое молоко."),
+        CoctailModel(name: "Мохито", alcohol: "Белый ром", amountOfAlcohol: 50,
+                     ingredients: ["свежая мята", "тростниковый коричневый сахар", "половинка лайма", "Спрайт", "лёд"],
+                     descrpiption: "Освежающий кубинский коктейль на основе лайма и мяты. Говорят, что этот напиток очень любил Эрнест Хемингуэй."),
+        CoctailModel(name: "Кровавая Мэри", alcohol: "Водка", amountOfAlcohol: 75,
+                     ingredients: ["томатный сок", "соль", "перец"],
+                     descrpiption: "Лидер продаж в русских барах. Готовится очень просто в домашних условиях.")
+    ]
+}
---
+++ PocketBartender/Models/CoctailModel.swift
@@ -0,0 +1,16 @@
+//
+//  CoctailModel.swift
+//  PocketBartender
+//
+//  Created by Pavel Parshin on 12.06.2020.
+//  Copyright © 2020 Pavel Parshin. All rights reserved.
+//
+
+
+struct CoctailModel {
+    let name: String
+    let alcohol: String
+    let amountOfAlcohol: Int
+    let ingredients: [String]
+    let descrpiption: String
+}
